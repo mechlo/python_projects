fddd0d413dd757f593c661f12a546170efb344e7
Matt Hunter <thehuntsman@gmail.com>
2018-08-02 16:23:00 -0700

    to transfer

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,21 @@
+class PagesController < ApplicationController
+  def index
+      @pages = Page.all
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = Page.create
+    render plain: params.to_json
+  end
+
+end
+
+
